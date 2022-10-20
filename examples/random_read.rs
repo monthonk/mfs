6c9a1e00a2a65c7cc5e02800aa17b7041ad2b908
Monthon Klongklaew <monthonk@amazon.co.uk>
2022-10-20 11:01:59 +0000

    Add random read example

---
+++ examples/random_read.rs
@@ -0,0 +1,21 @@
+use std::{fs::File, io::Read};
+use std::io::{SeekFrom, Seek};
+
+fn main() {
+    let mut f = File::open("mnt/hello.txt")
+        .expect("Should have been able to open the file");
+    
+    read(&mut f, 5, 5);
+    read(&mut f, 0, 5);
+}
+
+fn read(f: &mut File, pos: u64 , length: usize) -> usize {
+    f.seek(SeekFrom::Start(pos)).expect("Should be able to seek");
+
+    let mut contents = vec![0; length];
+    let byte_read = f.read(&mut contents)
+        .expect("Should have been able to read the file");
+
+    println!("read {byte_read} bytes from pos={pos}, contents={:?}", String::from_utf8(contents).unwrap());
+    return byte_read;
+}
