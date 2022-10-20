6b7abf105bbae732f828e0988813b2079d39f5a9
Monthon Klongklaew <monthonk@amazon.co.uk>
2022-10-20 09:49:50 +0000

    Implement first file system operation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+use async_trait::async_trait;
+use fuser::{
+    FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
+    Request,
+};
+use libc::ENOENT;
+use std::ffi::OsStr;
+use std::time::{Duration, UNIX_EPOCH};
+use std::path::PathBuf;
+
+const TTL: Duration = Duration::from_secs(1); // 1 second
+
+const HELLO_DIR_ATTR: FileAttr = FileAttr {
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::Directory,
+    perm: 0o755,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+
+const HELLO_TXT_CONTENT: &str = "Hello World!\n";
+
+const HELLO_TXT_ATTR: FileAttr = FileAttr {
+    ino: 2,
+    size: 13,
+    blocks: 1,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::RegularFile,
+    perm: 0o644,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+
+struct MFS;
+
+#[async_trait]
+impl Filesystem for MFS {
+    async fn getattr(&self, _req: &Request<'_>, ino: u64, reply: ReplyAttr) {
+        match ino {
+            1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
+            2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
+            _ => reply.error(ENOENT),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mountpoint = "mnt";
+    let mountpoint = PathBuf::from(mountpoint);
+    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    options.push(MountOption::AutoUnmount);
+
+    fuser::mount2(MFS, mountpoint, &options).unwrap();
 }
