75b7858c34240a35d35e963e61b7acd8f5e39caa
Monthon Klongklaew <monthonk@amazon.co.uk>
2022-10-20 15:45:55 +0000

    Change fs name

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 fn main() {
     let mountpoint = "mnt";
     let mountpoint = PathBuf::from(mountpoint);
-    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    let mut options = vec![MountOption::RO, MountOption::FSName("mfs".to_string())];
     options.push(MountOption::AutoUnmount);
 
     fuser::mount2(MFS, mountpoint, &options).unwrap();
