cbf1cb4b60832c0985782512f4ea6d9786257e1e
Monthon Klongklaew <monthonk@amazon.co.uk>
2022-10-20 21:34:56 +0000

    Implement real lookup

--- src/fs.rs
+++ src/fs.rs
@@ -7,6 +7,7 @@ use fuser::{
 use libc::ENOENT;
 use std::collections::HashMap;
 use std::ffi::OsStr;
+use std::sync::RwLock;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::{Duration, UNIX_EPOCH};
 
@@ -32,40 +33,25 @@ const HELLO_DIR_ATTR: FileAttr = FileAttr {
 
 const HELLO_TXT_CONTENT: &str = "Hello World!\n";
 
-const HELLO_TXT_ATTR: FileAttr = FileAttr {
-    ino: 2,
-    size: 13,
-    blocks: 1,
-    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-    mtime: UNIX_EPOCH,
-    ctime: UNIX_EPOCH,
-    crtime: UNIX_EPOCH,
-    kind: FileType::RegularFile,
-    perm: 0o644,
-    nlink: 1,
-    uid: 501,
-    gid: 20,
-    rdev: 0,
-    flags: 0,
-    blksize: 512,
-};
-
 pub struct MFS {
     client: Client,
     bucket_name: String,
-    ino_map: HashMap<u64, Inode>,
+    ino_map: RwLock<HashMap<u64, Inode>>,
     next_ino: AtomicU64,
 }
 
+#[derive(Debug)]
 struct Inode {
+    parent: u64,
     ino: u64,
     name: String,
     file_attr: FileAttr,
 }
 
 impl Inode {
-    pub fn new(ino: u64, name: String, object: &Object, kind: FileType) -> Inode {
+    pub fn new(parent: u64, ino: u64, name: String, object: &Object, kind: FileType) -> Inode {
         Inode {
+            parent,
             ino,
             name,
             file_attr: FileAttr {
@@ -92,20 +78,22 @@ impl Inode {
 impl MFS {
     pub fn new(client: Client, bucket_name: String) -> MFS {
         let root_ino = 1;
-        let mut ino_map = HashMap::new();
+        let mut ino_map: HashMap<u64, Inode> = HashMap::new();
         ino_map.insert(
             root_ino,
             Inode {
+                parent: 0,
                 ino: root_ino,
                 name: String::from(""),
                 file_attr: HELLO_DIR_ATTR,
             },
         );
+
         MFS {
             client,
             bucket_name,
-            ino_map,
-            next_ino: AtomicU64::new(root_ino),
+            ino_map: RwLock::new(ino_map),
+            next_ino: AtomicU64::new(root_ino + 1),
         }
     }
 
@@ -118,18 +106,21 @@ impl MFS {
 impl Filesystem for MFS {
     async fn lookup(&self, _req: &Request<'_>, parent: u64, name: &OsStr, reply: ReplyEntry) {
         println!("lookup parent={parent} name={:?}", name);
-        if parent == 1 && name.to_str() == Some("test") {
-            reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
-        } else if parent == 1 && name.to_str() == Some("s3-file-connector") {
-            reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
-        } else {
-            reply.error(ENOENT);
+        let ino_map_reader = self.ino_map.read().unwrap();
+        for (_, inode) in ino_map_reader.iter() {
+            dbg!(&inode);
+            if parent == inode.parent && name.to_str() == Some(&inode.name) {
+                reply.entry(&TTL, &inode.file_attr, 0);
+                return;
+            }
         }
+        reply.error(ENOENT);
     }
 
     async fn getattr(&self, _req: &Request<'_>, ino: u64, reply: ReplyAttr) {
         println!("getattr ino={ino}");
-        let inode = self.ino_map.get(&ino);
+        let ino_map_reader = self.ino_map.read().unwrap();
+        let inode = ino_map_reader.get(&ino);
         match inode {
             Some(node) => reply.attr(&TTL, &node.file_attr),
             None => reply.error(ENOENT),
@@ -191,9 +182,13 @@ impl Filesystem for MFS {
                     continue;
                 }
 
+                let parent = ino;
                 let next_ino = self.next_ino();
-                let entry = Inode::new(next_ino, String::from(name), obj, FileType::RegularFile);
-                entries.push(entry);
+                let mut ino_map_writer = self.ino_map.write().unwrap();
+                ino_map_writer.insert(next_ino, Inode::new(parent, next_ino, String::from(name), obj, FileType::RegularFile));
+
+                // FIXME: use inodes from MFS
+                entries.push(Inode::new(parent, next_ino, String::from(name), obj, FileType::RegularFile));
             }
 
             if let Some(next_token) = objects.next_continuation_token() {
