61c9a1d88314345e07cdc036e8f54e468dd4d239
Monthon Klongklaew <monthonk@amazon.co.uk>
2022-10-20 16:03:19 +0000

    Add s3 client to main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod fs;
 
+use aws_sdk_s3 as s3;
 use fs::MFS;
 use fuser::MountOption;
 use std::path::PathBuf;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), s3::Error> {
     let mountpoint = "mnt";
     let mountpoint = PathBuf::from(mountpoint);
     let mut options = vec![MountOption::RO, MountOption::FSName("mfs".to_string())];
     options.push(MountOption::AutoUnmount);
 
+    let config = aws_config::load_from_env().await;
+    let _ = s3::Client::new(&config);
+
     fuser::mount2(MFS, mountpoint, &options).unwrap();
+    Ok(())
 }
